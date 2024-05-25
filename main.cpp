797613352dfc0c52f3bb7efb6bef78adc4c892dc
Kacper Doga <doga.kacper@gmail.com>
2024-05-25 15:01:58 +0200

    convert input to graph

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
-#include <iostream>
+#include "include/Vector.h"
+#include "include/Graph.h"
+
+size_t readNextNumber() {
+    size_t value = 0;
+    char in = 0;
+
+    while (in != ' ' && in != '\n') {
+        in = (char) getchar();
+
+        if (in < '0' || in > '9')
+            continue;
+
+        value *= 10;
+        value += in-'0';
+    }
+
+    return value;
+}
+
+Vector<Vector<int>>& readVertexes(size_t n) {
+    auto* data = new Vector<Vector<int>>(n);
+    while (n) {
+        size_t size = readNextNumber();
+        auto* vertex = new Vector<int>(size);
+
+        while (size) {
+            int value = (int) readNextNumber();
+            vertex->push_back(value);
+            size--;
+        }
+        data->push_back(*vertex);
+        n--;
+    }
+    return *data;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    size_t graphs = readNextNumber();
+    while (graphs) {
+        size_t n = readNextNumber();
+        auto* vertices = &readVertexes(n);
+        auto* graph = new Graph(*vertices);
+        graph->degreeSequence();
+        graphs--;
+    }
     return 0;
 }
